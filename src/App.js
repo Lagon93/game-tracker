2abfb6aa3712d80032720827b02fd70ec32e2773
Lagon <teixgarcia@gmail.com>
2024-05-23 22:54:45 +0200

    Navbar y caratulas

--- src/App.js
+++ src/App.js
@@ -2,14 +2,15 @@ import React, { useState, useEffect } from 'react';
 import supabase from './supabase';
 import './App.css';
 
-
 function App() {
   const [data, setData] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
+  const [darkMode, setDarkMode] = useState(false);
+  const [sortConfig, setSortConfig] = useState({ key: '', direction: 'ascending' });
 
   useEffect(() => {
     async function fetchData() {
       try {
-        // Consultar los datos de la tabla en Supabase
         const { data, error } = await supabase.from('Juegos_duplicate').select('*');
         console.log(data);
 
@@ -26,38 +27,109 @@ function App() {
     fetchData();
   }, []);
 
+  const totalAchievements = data.reduce((acc, item) => {
+    const logros = parseInt(item.Logros, 10);
+    return acc + (isNaN(logros) ? 0 : logros);
+  }, 0);
+
+
+  const toggleDarkMode = () => setDarkMode(!darkMode);
+
+   const sortedData = React.useMemo(() => {
+    let sortableData = [...data];
+    if (sortConfig.key) {
+      sortableData.sort((a, b) => {
+        let aValue = a[sortConfig.key];
+        let bValue = b[sortConfig.key];
+
+        if (sortConfig.key === 'Fecha de finalización') {
+          aValue = aValue ? new Date(aValue) : new Date(0);
+          bValue = bValue ? new Date(bValue) : new Date(0);
+        } else if (sortConfig.key === 'Logros') {
+          aValue = parseInt(aValue) || 0;
+          bValue = parseInt(bValue) || 0;
+        }
+
+        if (aValue < bValue) {
+          return sortConfig.direction === 'ascending' ? -1 : 1;
+        }
+        if (aValue > bValue) {
+          return sortConfig.direction === 'ascending' ? 1 : -1;
+        }
+        return 0;
+      });
+    }
+    return sortableData;
+  }, [data, sortConfig]);
+
+  const requestSort = key => {
+    let direction = 'ascending';
+    if (sortConfig.key === key && sortConfig.direction === 'ascending') {
+      direction = 'descending';
+    } else if (sortConfig.key === key && sortConfig.direction === 'descending') {
+      direction = 'ascending';
+    }
+    setSortConfig({ key, direction });
+  };
+
   return (
-    <div className="glass-container"> {/* Añade la clase 'glass-container' para aplicar los estilos */}
-      <h1>Tabla de Datos desde Supabase</h1>
-      <table className="glass-table"> {/* Añade la clase 'glass-table' para aplicar los estilos */}
-        <thead>
-          <tr>
-            <th>Juego</th>
-            <th>Fecha de finalización</th>
-            <th>Logros</th>
+    <div className={`app ${darkMode ? 'dark-mode' : ''}`}>
+      <div className="header">
+        <div className="stats">
+          <span>🎮 {data.length}</span>
+          <span>🏆 {totalAchievements}</span>
+        </div>
+        <h1>GAME TRACKER</h1>
+        <div className="controls">
+          <div className="search-input"><input 
+            type="text" 
+            placeholder="Buscar Juego..." 
+            value={searchTerm} 
+            onChange={(e) => setSearchTerm(e.target.value)} 
+          /></div>
+            <label className="switch">
+              <input type="checkbox"/>
+              <div className="slider"></div>
+              <div className="bg"></div>
+            </label>
+        </div>
+      </div>
+      <div className="glass-container">
+        <table className="glass-table">
+          <thead>
+            <tr>
+              <th>Caratula Juego</th>
+              <th onClick={() => requestSort('Juego')}>Juego</th>
+            <th onClick={() => requestSort('Fecha de finalización')}>Fecha de finalización</th>
+            <th onClick={() => requestSort('Logros')}>Logros</th>
             <th>Plataforma</th>
-            <th>Horas</th>
-            <th>Nota</th>
-            <th>Recomendado</th>
-          </tr>
-        </thead>
-        <tbody>
-          {data.map((item, index) => (
-            <tr key={index}>
-              <td>{item.Juego}</td>
-              <td>{item['Fecha de finalización']}</td>
-              <td>{item.Logros}</td>
-              <td>{item.Plataforma}</td>
-              <td>{item.Horas}</td>
-              <td>{item.Nota}</td>
-              <td>{item.Recomendado}</td>
+            <th onClick={() => requestSort('Horas')}>Horas</th>
+            <th onClick={() => requestSort('Nota')}>Nota</th>
+              <th>Recomendado</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+          <tbody>
+            {sortedData.map((item, index) => (
+              <tr key={index}>
+                <td><img src={item['Caratula Juego']} alt={item.Juego} style={{ width: '100px', height: 'auto' }} />  </td>              
+                <td>{item.Juego}</td>
+                <td>{item['Fecha de finalización']}</td>
+                <td>{item.Logros}</td>
+                <td>{item.Plataforma}</td>
+                <td>{item.Horas}</td>
+                <td>{item.Nota}</td>
+                <td>  {item.Recomendado === 'Si' ? (
+                  <span className="approved">✓</span>
+                ) : (
+                  <span className="not-approved">✗</span>
+                )}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 }
-
 
 export default App;
